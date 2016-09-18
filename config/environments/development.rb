6eb38a9ce91980eed2c23cae602bf8d9c7050e6b
Chris Eady <chris.eady1@gmail.com>
2016-09-17 20:03:27 -0500

    added user login and authentication with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Use letter_opener gem for mail delivery.
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -51,4 +54,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Configure mailer for devise gem.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
